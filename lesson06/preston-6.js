43a699413a439b23a1653ab21fbc887d69c2fc40
Glen Meredith <mer16014@byui.edu>
2021-07-09 17:58:41 -0600

    started w6

---
+++ lesson06/preston-6.js
@@ -0,0 +1,80 @@
+// window.addEventListener('load', () => {
+//     const hambutton = document.querySelector('.ham');
+//     const mainnav = document.querySelector('#navigation');
+
+//     hambutton.addEventListener('click', () => {
+//         mainnav.classList.toggle('responsive')
+//     }, false);
+// });
+
+function toggleMenu() {
+    document.getElementsByClassName('navigation')[0].classList.toggle('responsive');
+}
+
+window.onresize = () => {if (window.innerWidth > 760) document.getElementsByClassName('navigation')[0].classList.remove('responsive')};
+
+function getDate() {
+    let year = new Date().getFullYear();
+    let month = new Date().getMonth();
+    if (month == 0) {
+        month = 'January'
+    }
+    if (month == 1) {
+        month = 'February'
+    }
+    if (month == 2) {
+        month = 'March'
+    }
+    if (month == 3) {
+        month = 'April'
+    }
+    if (month == 4) {
+        month = 'May'
+    }
+    if (month == 5) {
+        month = 'June'
+    }
+    if (month == 6) {
+        month = 'July'
+    }
+    if (month == 7) {
+        month = 'August'
+    }
+    if (month == 8) {
+        month = 'September'
+    }
+    if (month == 9) {
+        month = 'October'
+    }
+    if (month == 10) {
+        month = 'November'
+    }
+    if (month == 11) {
+        month = 'December'
+    }
+    let dayOfMonth = new Date().getDate();
+    let weekdayNumber = new Date().getDay();
+    var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    var weekday = daysOfWeek[weekdayNumber]
+    document.getElementById("outputspan").innerHTML = weekday + ", " + dayOfMonth + " " + month + " " + year;
+    return weekday;
+}
+getDate();
+
+function pancakeBanner() {
+    let weekday = getDate();
+    if (weekday == 'Friday') {
+        document.getElementsByClassName('pancakegrid')[0].style.display = 'grid';
+    }
+}
+pancakeBanner();
+
+windchill = () => {
+    let temp = parseInt(document.getElementById('currenttemp').value);
+    let windspeed = parseInt(document.getElementById('windspeed').value)
+    let chilltemp, roundtemp;
+    chilltemp = 35.74 + 0.6615 * temp - 35.75 * Math.pow(windspeed, .016) + 0.4275 * temp * Math.pow(windspeed, .016);
+    roundtemp = Math.round(chilltemp*100)/100;
+    document.getElementById('windchill').innerHTML = roundtemp;
+}
+windchill();
