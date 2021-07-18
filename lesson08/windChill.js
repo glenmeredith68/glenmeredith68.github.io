6603d67540a26fea13541d789cc85d8576bae17e
Glen Meredith <mer16014@byui.edu>
2021-07-18 02:14:32 -0600

    finished storm center

---
+++ lesson08/windChill.js
@@ -0,0 +1,13 @@
+windchill = () => {
+    let temp = parseFloat(document.getElementById('currenttemp').textContent);
+    let windspeed = parseFloat(document.getElementById('windspeed').textContent);
+    let chilltemp, roundtemp;
+    if (temp <= 60) {
+        chilltemp = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed, .16)) + (0.4275 * temp * Math.pow(windspeed, .16));
+        roundtemp = Math.round(chilltemp * 100) / 100;
+     
+    } else {
+         roundtemp = temp}
+    document.getElementById('windchill').innerHTML = roundtemp;
+}
+windchill();
